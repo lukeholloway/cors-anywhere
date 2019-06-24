22a80d8d02fad9d9018c0c4b1207f68aee364cba
Luke Holloway <holloway.creations@gmail.com>
2019-06-24 11:13:13 +0100

    Changed whitelist

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELI
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
-  originWhitelist: originWhitelist,
+  // originWhitelist: originWhitelist,
+  originWhitelist: ['https://lucecodev.wpengine.com'],
   requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
