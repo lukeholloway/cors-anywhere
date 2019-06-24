793d6da798a85dda378a22eaf4bab68c7f15191c
Luke Holloway <holloway.creations@gmail.com>
2019-06-24 12:49:13 +0100

    Updated server

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELI
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
-  originWhitelist: originWhitelist,
+  //originWhitelist: originWhitelist,
+  originWhitelist: ['https://luceco-test.herokuapp.com', 'https://luceco.com', 'https://luceco-cors.herokuapp.com'],
   requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
